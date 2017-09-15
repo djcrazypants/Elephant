bfb264087ee1cb80b7710de0f1d3cbaa3e987172
djcrazypants <djcrazypants@gmail.com>
2017-09-15 17:21:29 +1000

    clean up
    cleaned up the code

--- Player.gd
+++ Player.gd
@@ -9,9 +9,7 @@ var bullet
 var bullet_velocity = Vector2()
 var bullet_cooldown = 0
 
-
-func _fixed_process(delta):
-	
+func _fixed_process(delta):	
 	
 	#poll player input and set values accordingly
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_left"):
@@ -20,34 +18,40 @@ func _fixed_process(delta):
 		velocity.y = -1
 		last_direction.x += -1
 		last_direction.y += -1
+		
 	elif Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_right"):
 		get_node("Aim Ray").rotation_deg = 225
 		velocity.x = 1
 		velocity.y = -1
 		last_direction.x = 1
 		last_direction.y = -1
+		
 	elif Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_left"):
 		get_node("Aim Ray").rotation_deg = 45
 		velocity.x = -1
 		velocity.y = 1
 		last_direction.x = -1
 		last_direction.y = 1
+		
 	elif Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_right"):
 		get_node("Aim Ray").rotation_deg = 315
 		velocity.y = 1
 		velocity.x = 1
 		last_direction.y = 1
 		last_direction.x = 1
+		
 	elif Input.is_action_pressed("ui_up"):
 		get_node("Aim Ray").rotation_deg = 180
 		velocity.y = -1
 		last_direction.y = -1
 		last_direction.x = 0
+		
 	elif Input.is_action_pressed("ui_down"):
 		get_node("Aim Ray").rotation_deg = 0
 		velocity.y = 1
 		last_direction.y = 1
 		last_direction.x = 0
+		
 	elif Input.is_action_pressed("ui_right"):
 		get_node("Aim Ray").rotation_deg = 270
 		velocity.x = 1
@@ -65,19 +69,17 @@ func _fixed_process(delta):
 		$ElephantSprite.set_flip_h(true)
 		$Uzi.set_flip_h(true)
 		$Uzi.position.x = -120
+		
 	else:
 		velocity = Vector2(0,0) #do not adjust last_direction in this case
-	
-	
-	
+		
 	if Input.is_action_pressed("ui_accept") and bullet_cooldown <= 0:
 		
 		var bullet = preload("res://Bullet.tscn").instance()
 		bullet.add_collision_exception_with(self)
 		get_parent().add_child( bullet ) #don't want bullet to move with me, so add it as child of parent
 		bullet.position = self.position+(last_direction*5) #set origin to player so angle and collision will be correct
-		bullet.velocity = last_direction
-		
+		bullet.velocity = last_direction		
 		
 		bullet_cooldown = 10
 	
